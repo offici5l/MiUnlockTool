478eefb27b0a4bb64ba54d9b6287721fb2165d09
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-14 16:54:50 +0100

    Update

---
+++ setup.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.run(["pip", "install", "requests"])
+subprocess.run(["pip3", "install", "pyshorteners"])
+subprocess.run(["pip3", "install", "pycryptodomex"])
+subprocess.run(["pip", "install", "termcolor"])
