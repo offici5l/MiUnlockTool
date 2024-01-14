1e6f6f272cda95692a30cf6217454e598dac5c4d
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-14 18:24:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import subprocess
 
-subprocess.run(["pip", "install", "requests"])
+subprocess.run(["python3", "-m", "ensurepip"])
+subprocess.run(["pip3", "install", "requests"])
 subprocess.run(["pip3", "install", "pyshorteners"])
 subprocess.run(["pip3", "install", "pycryptodomex"])
 subprocess.run(["pip", "install", "termcolor"])
