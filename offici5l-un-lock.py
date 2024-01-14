4dadd9b4a30f990b6941cb634f551c6e5caa8c44
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-14 19:22:17 +0100

    Update offici5l-un-lock.py

--- offici5l-un-lock.py
+++ offici5l-un-lock.py
@@ -10,6 +10,7 @@ import webbrowser
 filename = "account_info.txt"
 
 
+
 print("\n\033[92m1.\033[0m Unlock")
 print("\033[92m2.\033[0m Lock")
 
@@ -221,7 +222,7 @@ if "code" in result and result["code"] == 10000:
         lines = [line for line in file.readlines() if "token:" not in line and "product:" not in line]
     with open(filename, "w") as file:
         file.writelines(lines)
-    exec("\n".join(line for i, line in enumerate(codecs.open('un-lock.py', 'r', 'utf-8').read().split('\n'), 1) if i not in range(28, 63)))
+    exec("\n".join(line for i, line in enumerate(codecs.open('un-lock.py', 'r', 'utf-8').read().split('\n'), 1) if i not in range(12, 45)))
     exit()
 
 if "encryptData" in result:
