d3384d860cfe23d907840b8b12d14cad7ee9458d
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-25 01:36:03 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(["python3", "-m", "ensurepip"])
-subprocess.run(["pip3", "install", "requests"])
-subprocess.run(["pip3", "install", "pycryptodomex"])
-subprocess.run(["pip", "install", "termcolor"])
